0c2b2566eba3cbb0ec9cba6d1953deb16b004c0c
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-03-07 21:49:16 +0530

    replace class to className

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function () {
   return (
     <div>
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
         <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     </div>
   )
