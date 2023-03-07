af38d072e4cca203d296ab5662426f5ea18c3e5f
Akhil Binoy <92171031+akhil888binoy@users.noreply.github.com>
2023-03-07 14:17:04 +0530

    customised navbar

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,10 +6,16 @@ export default function TextForm(props) {
     let newText= text.toUpperCase();
     setText(newText);
   }
+  const handleLowClick =()=>{
+    console.log("Upper case was clicked");
+    let newText= text.toLowerCase();
+    setText(newText);
+  }
   const handleOnChange =(event)=>{
     console.log("Onchanged");
     setText(event.target.value);
   }
+  
   const [text, setText] = useState('Enter text here');
   
   return (
@@ -21,12 +27,16 @@ export default function TextForm(props) {
                 <div className="form-group">
                     <textarea type="email" className="form-control" id="mybox"  value={text} onChange={handleOnChange} rows = "8"/>
                 </div>
-                <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to lowercase</button>
             </div>
             
         <div className="container my-3">
             <h1>Your Text Summary</h1>
             <p> {text.split(" ").length} words {text.length} characters</p>
+            <p> {0.008 * text.split(" ").length } Minutes read</p>
+            <h2> Preview </h2>
+            <p>{text}</p>
         </div>
     
     </>
